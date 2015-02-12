33250b71a629bc78719067e4611d7ca4ccf57b22
José Pedro <jose.pedro.dias@gmail.com>
2015-02-12 16:17:49 +0000

    improving calc safety

--- plugin-calc.js
+++ plugin-calc.js
@@ -1,17 +1,21 @@
 plugins.push({
 	onNewMessage: function(msg) {
+		'use strict';
+
+		var fs, require, window, console, open, process;
+
 		/*jshint evil:true */
 		if (msg.text.substring(0, 5) !== '!calc') { return; }
 		
 		var expr = msg.text.substring(5).trim();
 		try {
-			expr = expr
+			/*expr = expr
 				.replace(/alert/g,   '')
 				.replace(/confirm/g, '')
 				.replace(/open/g,    '')
 				.replace(/prompt/g,  '')
 				.replace(/process/g, '')
-				.replace(/require/g, '');
+				.replace(/require/g, '');*/
 
 			var result = eval(expr);
 			api.say({
